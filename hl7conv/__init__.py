23501a31d04a950899dece36738eb43914793ebc
unknown <kalosha.ilya@yandex.ru>
2023-01-21 17:12:07 +0300

    add code

--- hl7conv/__init__.py
+++ hl7conv/__init__.py
@@ -1,10 +1,10 @@
 import logging
 
-from .hl7json import HL7JSON
-from .schemas.enums import Versions
+from .hl7json import Hl7Json
+from .schemas import versions
 
 
-__all__ = ["Hl7Json", "Versions"]
+__all__ = ["Hl7Json", "versions"]
 
 # setup library logging
 logging.getLogger(__name__).addHandler(logging.NullHandler())
